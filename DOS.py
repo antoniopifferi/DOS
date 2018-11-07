271c40d960ebf2b0a440f67c9f9815d50cb30b52
Antonio Pifferi <antonio.pifferi@polimi.it>
2018-11-07 16:55:41 +0100

    Version Lab after MAYBE first measures

--- DOS.py
+++ DOS.py
@@ -9,14 +9,15 @@ from scipy import *
 from matplotlib.pyplot import *
 from pandas import *
 
-data=read_table('D:\Beta\Analysis\Maybe\MAYm0000.txt')
-Trim=read_table('D:\Beta\Analysis\Maybe\Trim.txt')
+data=read_table('W:\Analysis\Maybe\MAYm0000.txt')
+Trim=read_table('W:\Analysis\Maybe\Trim.txt')
 data=merge(data,Trim)
 for ir in data.Rho.unique():
-    table=data[(data.Det==data.Trim) & (data.Material=='Phantom')].pivot_table(['Mua','Mus'],index='Lambda',columns=['Sample','Pos'],aggfunc='mean')
+#    table=data[(data.Det==data.Trim)& (data.Sample=='AP') & (data.Pos=='Abdomen') & (data.Rho==ir)].pivot_table(['Mua','Mus'],index='Lambda',columns=['Sample','Pos','Meas'],aggfunc='mean')
+    table=data[(data.Det==data.Trim) & (data.Pos=='Muscle')].pivot_table(['Mua','Mus'],index='Lambda',columns=['Sample','Pos','Rho'],aggfunc='mean')
     figure('Mua'+str(ir))
     plot(table.Mua)
-    ylim([0,0.4]), legend(table.Mua), title('Mua'+str(ir)), xlabel('wavelength (nm)'), ylabel('absorption (cm-1)')
+    ylim([0,1.0]), legend(table.Mua), title('Mua'+str(ir)), xlabel('wavelength (nm)'), ylabel('absorption (cm-1)')
     figure('Mus'+str(ir))
     plot(table.Mus)
-    ylim([0,10.0]), legend(table.Mus), title('Mus'+str(ir)), xlabel('wavelength (nm)'), ylabel('reduced scattering (cm-1)')
+    ylim([0,15.0]), legend(table.Mus), title('Mus'+str(ir)), xlabel('wavelength (nm)'), ylabel('reduced scattering (cm-1)')
