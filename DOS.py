39c0bb12b7cd2b1a045c14128096514e13dceb45
Antonio Pifferi <antonio.pifferi@polimi.it>
2018-11-23 18:29:39 +0100

    after MAYBE (first experiment) Analysis

--- DOS.py
+++ DOS.py
@@ -8,16 +8,60 @@ from numpy import *
 from scipy import *
 from matplotlib.pyplot import *
 from pandas import *
+close('all')
 
-data=read_table('W:\Analysis\Maybe\MAYm0000.txt')
-Trim=read_table('W:\Analysis\Maybe\Trim.txt')
+# PARAMETERS
+PathAnalysis='D:\\Beta\\Analysis\\Maybe\\'
+DirFigure='Figure\\'
+FileData='MAYm0000_ref01.txt'
+FileTrim='Trim.txt'
+FileComponents='Components2.txt'
+PathFigure=PathAnalysis+DirFigure
+
+# LOAD COMPONENTS
+Components=read_table(PathAnalysis+FileComponents)
+Components.plot(x='Lambda')
+yscale('log')
+ylim([0,0.5]), title('Components'), xlabel('wavelength (nm)'), ylabel('specific absorption (cm-1)')
+savefig(PathFigure+'Components.jpg')
+show()
+
+# LOAD DATA
+data=read_table(PathAnalysis+FileData)
+Trim=read_table(PathAnalysis+FileTrim)
 data=merge(data,Trim)
-for ir in data.Rho.unique():
-#    table=data[(data.Det==data.Trim)& (data.Sample=='AP') & (data.Pos=='Abdomen') & (data.Rho==ir)].pivot_table(['Mua','Mus'],index='Lambda',columns=['Sample','Pos','Meas'],aggfunc='mean')
-    table=data[(data.Det==data.Trim) & (data.Pos=='Muscle')].pivot_table(['Mua','Mus'],index='Lambda',columns=['Sample','Pos','Rho'],aggfunc='mean')
-    figure('Mua'+str(ir))
-    plot(table.Mua)
-    ylim([0,1.0]), legend(table.Mua), title('Mua'+str(ir)), xlabel('wavelength (nm)'), ylabel('absorption (cm-1)')
-    figure('Mus'+str(ir))
-    plot(table.Mus)
-    ylim([0,15.0]), legend(table.Mus), title('Mus'+str(ir)), xlabel('wavelength (nm)'), ylabel('reduced scattering (cm-1)')
+
+# PLOT DATA
+for ip in data.Pos.unique():
+    for ir in data.Rho.unique():
+        table=data[(data.Det==data.Trim)&(data.Pos==ip)].pivot_table(['Mua','Mus'],index='Lambda',columns=['Sample','Meas'],aggfunc='mean')
+        namefig='Mua_'+ip+'_'+str(ir)
+        figure(namefig)
+        plot(table.Mua)
+        grid(True)
+        title(ip+' - Rho='+str(ir))
+        ylim([0,0.6]), legend(table.Mua), xlabel('wavelength (nm)'), ylabel('absorption (cm-1)')
+        savefig(PathFigure+namefig+'.jpg')
+        show()
+        
+        namefig='Mus_'+ip+'_'+str(ir)
+        figure(namefig)
+        plot(table.Mus)
+        grid(True)
+        title(ip+' - Rho='+str(ir))
+        ylim([0,15.0]), legend(table.Mus), xlabel('wavelength (nm)'), ylabel('reduced scattering (cm-1)')
+        savefig(PathFigure+namefig+'.jpg')
+        show() 
+
+        comp=Components[Components['Lambda'].isin(data.Lambda.unique())].values
+        comp=delete(comp,0,1)
+        aComp=linalg.lstsq(comp[2:],table.Mua[2:],rcond=None)[0]
+        dfComp=DataFrame(data=aComp.transpose(),
+                         columns=Components.columns[1:],
+                         index=table.Mua.columns)
+        dfComp['tHb']=dfComp['HHb']+dfComp['O2Hb']
+        dfComp['SO2']=dfComp['O2Hb']/dfComp['tHb']
+        dfComp = dfComp.applymap("{0:.2f}".format)
+        print('SECTION ON '+ip+' - Rho='+str(ir))
+        display(dfComp)
+        dfComp.to_csv(PathFigure+namefig+'.csv')
