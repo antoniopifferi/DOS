a15d25ad5b594b71b4b9f31b0b35c87330aeeef0
Antonio Pifferi <antonio.pifferi@polimi.it>
2019-02-14 00:23:28 +0100

    MAYBE muscle

--- DOS.py
+++ DOS.py
@@ -11,10 +11,14 @@ from pandas import *
 close('all')
 
 # OPTIONS
-SHOW_SINGLE=True
+PLOT_SINGLE=False
+PLOT_MULTI=False
 SHOW_TABLE=True
+PLOT_COMP=True
+#Task='Abdomen'
+Task='Muscle'
 Opt=['Mua','Mus']
-YLIM={'Mua':[0,0.5],'Mus':[0,20]}
+YLIM={'Mua':[0,0.7],'Mus':[0,20]}
 YLABEL={'Mua':'absorption (cm-1)','Mus':'reduced scattering (cm-1)'}
 
 # CONVERSION FUNCTION
@@ -28,9 +32,17 @@ def cm2inch(*tupl):
 # PARAMETERS
 PathAnalysis='D:\\Beta\\Analysis\\Maybe\\'
 DirFigure='Figure\\'
-FileData='MAYm0023.txt'
-FileTrim='trimMAYm0023.txt'
-FileKey='keyMAYm0023.txt'
+if Task=='Muscle':
+    FileData='MAYm0023.txt'
+    FileTrim='trimMAYm0023.txt'
+    FileKey='keyMAYm0023.txt'
+    FIRST_LAMBDA=0
+else:
+    FileData='MAYm0024.txt'
+    FileTrim='trimMAYm0024.txt'
+    FileKey='keyMAYm0024.txt'
+    FIRST_LAMBDA=2
+    
 FileComponents='Components2.txt'
 PathFigure=PathAnalysis+DirFigure
 
@@ -54,23 +66,38 @@ rawData=merge(rawData,Trim)
 filtData=rawData[(rawData.Det==rawData.Trim)&(rawData.Accept=='OK')]
 
 # PLOT SINGLE
-if SHOW_SINGLE:
+if PLOT_SINGLE:
     for iss in filtData.Subject.unique():
-        for ip in filtData.Pos.unique():
+        for im in filtData.Meas.unique():
             fig=figure(figsize=cm2inch(40, 15))
-            table=filtData[(filtData.Subject==iss)&(filtData.Pos==ip)].pivot_table(Opt,index='Lambda',columns=['Rho'],aggfunc='mean')
+            table=filtData[(filtData.Subject==iss)&(filtData.Meas==im)].pivot_table(Opt,index='Lambda',columns=['Rho'],aggfunc='mean')
             for io in Opt:
                 fig.add_subplot(1,2,1+Opt.index(io))
                 plot(table[io])
                 ylim(YLIM[io]), legend(table[io]), xlabel('wavelength (nm)'), ylabel(YLABEL[io])
-                title('Subject #'+str(iss)+' - Pos='+str(ip))
+                title('Subject #'+str(iss)+' - Meas='+str(im))
                 grid(True)
-show()
-        
-table=filtData.pivot_table(Opt,index='Lambda',columns=['Subject','Pos','Rho'],aggfunc='mean')
+    show()
+
+if PLOT_MULTI:
+    for io in Opt:
+        for iss in filtData.Subject.unique():
+            fig=figure(figsize=cm2inch(40, 15))
+            Rho=list(filtData.Rho.unique())
+            for ir in Rho:
+                table=filtData[(filtData.Subject==iss)&(filtData.Rho==ir)].pivot_table(Opt,index='Lambda',columns=['Meas'],aggfunc='mean')
+                fig.add_subplot(1,len(Rho),1+Rho.index(ir))
+                plot(table[io])
+                ylim(YLIM[io]), legend(table[io]), xlabel('wavelength (nm)'), ylabel(YLABEL[io])
+                title('Subject #'+str(iss)+' - Rho='+str(ir))
+                grid(True)
+    show()
+
+
+table=filtData.pivot_table(Opt,index='Lambda',columns=['Subject','Rho','Meas'],aggfunc='mean')
 comp=Components[Components['Lambda'].isin(filtData.Lambda.unique())].values
 comp=delete(comp,0,1)
-aComp=linalg.lstsq(comp[2:],table.Mua[2:],rcond=None)[0]
+aComp=linalg.lstsq(comp[FIRST_LAMBDA:],table.Mua[FIRST_LAMBDA:],rcond=None)[0]
 dfComp=DataFrame(data=aComp.transpose(),columns=Components.columns[1:],index=table.Mua.columns)
 dfComp['tHb']=dfComp['HHb']+dfComp['O2Hb']
 dfComp['SO2']=dfComp['O2Hb']/dfComp['tHb']
