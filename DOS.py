86b79f852859d46e2e7365587a9856e10ca463a0
Antonio Pifferi <antonio.pifferi@polimi.it>
2019-03-12 07:09:47 +0100

    Merged with SpectralFit datatbase

--- DOS.py
+++ DOS.py
@@ -11,15 +11,16 @@ from pandas import *
 close('all')
 
 # OPTIONS
-PLOT_SINGLE=True
+PLOT_SINGLE=False
 PLOT_MULTI=False
-SHOW_TABLE=True
+SHOW_TABLE=False
 PLOT_COMP=False
-Task='Abdomen'
-#Task='Muscle'
+#Task='Abdomen'
+Task='Muscle'
 Opt=['Mua','Mus']
 YLIM={'Mua':[0,0.5],'Mus':[0,20]}
 YLABEL={'Mua':'absorption (cm-1)','Mus':'reduced scattering (cm-1)'}
+COMP_LIST=['HHb','O2Hb','tHb','SO2','Lipid','H2O','Coll','Tot']
 
 # CONVERSION FUNCTION
 def cm2inch(*tupl):
@@ -34,6 +35,7 @@ PathAnalysis='D:\\Beta\\Analysis\\Maybe\\'
 DirFigure='Figure\\'
 if Task=='Muscle':
     FileData='MAYm0023.txt'
+    FileDataSpect='MAYm0023_Spect.txt'
     FileTrim='trimMAYm0023.txt'
     FileKey='keyMAYm0023.txt'
     FIRST_LAMBDA=0
@@ -66,6 +68,31 @@ rawData.rename(columns=dcKey,inplace=True)
 Trim=read_table(PathAnalysis+FileTrim)
 rawData=merge(rawData,Trim)
 filtData=rawData[(rawData.Det==rawData.Trim)&(rawData.Accept=='OK')]
+
+# CALCULATE COMPONENTS
+table=filtData.pivot_table(Opt,index='Lambda',columns=['Subject','Meas','Rho'],aggfunc='mean')
+comp=Components[Components['Lambda'].isin(filtData.Lambda.unique())].values
+comp=delete(comp,0,1)
+aComp=linalg.lstsq(comp[FIRST_LAMBDA:],table.Mua[FIRST_LAMBDA:],rcond=None)[0] #[0] to extract m-coeff in lstsq
+dfComp=DataFrame(data=aComp.transpose(),columns=Components.columns[1:],index=table.Mua.columns)
+dfComp['tHb']=dfComp['HHb']+dfComp['O2Hb']
+dfComp['SO2']=dfComp['O2Hb']/dfComp['tHb']
+dfComp['Tot']=dfComp['Lipid']+dfComp['H2O']+dfComp['Coll']
+dfComp['FitComp']='LambdaFit'
+filtData=merge(filtData,dfComp,on=['Subject','Meas','Rho'])
+
+# LOAD SPECT DATA
+rawDataSpect=read_table(PathAnalysis+FileDataSpect)
+rawDataSpect.drop_duplicates(inplace=True)
+#filtData=merge(filtData,rawDataSpect,on=['Subject','Meas','Rho','FitComp'],how='left')
+filtData=concat([filtData,rawDataSpect])
+
+# CALCULATE DERIVED COMPONENTS
+filtData['tHb']=filtData['HHb']+filtData['O2Hb']
+filtData['SO2']=filtData['O2Hb']/filtData['tHb']
+filtData['Tot']=filtData['Lipid']+filtData['H2O']+filtData['Coll']
+
+
 
 # PLOT SINGLE
 if PLOT_SINGLE:
@@ -104,17 +131,7 @@ if PLOT_MULTI:
     show()
 
 
-table=filtData.pivot_table(Opt,index='Lambda',columns=['Subject','Rho'],aggfunc='mean')
-#table=filtData.pivot_table(Opt,index='Lambda',columns=['Subject','Rho','Meas'],aggfunc='mean')
-comp=Components[Components['Lambda'].isin(filtData.Lambda.unique())].values
-comp=delete(comp,0,1)
-aComp=linalg.lstsq(comp[FIRST_LAMBDA:],table.Mua[FIRST_LAMBDA:],rcond=None)[0]
-dfComp=DataFrame(data=aComp.transpose(),columns=Components.columns[1:],index=table.Mua.columns)
-dfComp['tHb']=dfComp['HHb']+dfComp['O2Hb']
-dfComp['SO2']=dfComp['O2Hb']/dfComp['tHb']
-dfComp['Tot']=dfComp['Lipid']+dfComp['H2O']+dfComp['Coll']
-#dfComp['Tot']=dfComp['Lipid']+dfComp['H2O']
-
+# Show Components
 pandas.options.display.max_columns = None
 pandas.options.display.max_rows = None
 pandas.options.display.max_colwidth = 10
@@ -124,5 +141,18 @@ pandas.options.display.precision = 2
 display(dfComp)
 dfComp = dfComp.reset_index()
 dfComp.to_csv(PathAnalysis+'dfComp.csv', index=False)
-
 #dfComp.T.plot(subplots=True,layout=[10,40],figsize=[20,10])
+
+# Show Components from filtData
+table=filtData[filtData.FitComp=='LambdaFit'].pivot_table(index=['Subject','Rho'],columns=['SO2','H2O'],aggfunc='mean')
+#display(filtData)
+
+# PLOT COMPONENTS
+filtData[COMP_LIST].fillna(0)
+for ifc in filtData.FitComp.unique():
+    for ic in COMP_LIST:
+        table=filtData[filtData.FitComp==ifc].pivot_table(ic,index='Meas',columns=['Rho','Subject'],aggfunc='mean')
+        numRho=len(filtData.Rho.unique())
+        numSubject=len(filtData.Subject.unique())
+        table.plot(subplots=True,layout=[numRho,numSubject],sharex=True,sharey=True,title=ic+', '+ifc)
+        subplots_adjust(wspace=0, hspace=0)
